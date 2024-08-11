8540fe73d24a3af8abb327eae3e80c2aedf9235c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-08-11 09:11:50 +0300

    suppression des fichiers de test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NavComponent } from './accueil/nav/nav.component';
 import { ProduitsComponent } from './accueil/produits/produits.component';
 import { CategorieComponent } from './accueil/produits/categorie/categorie.component';
 import { CarteComponent } from './accueil/produits/carte/carte.component';
-import { AdminDashboardComponent } from './views/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component:AccueilComponent },
-  { path: 'admindashboard', component:AdminDashboardComponent },
 ]
 
 @NgModule({
@@ -23,7 +19,7 @@ const routes: Routes = [
     NavComponent,
     ProduitsComponent,
     CategorieComponent,
-    CarteComponent
+    CarteComponent,
   ],
   imports: [
     BrowserModule,
