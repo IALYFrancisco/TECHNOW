8fce9f549c256f049579fa4a0eee22ec7dd78f49
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-08-18 13:07:57 +0300

    ajout des éléments de filtre color

--- src/app/accueil/produits/categorie/categorie.component.ts
+++ src/app/accueil/produits/categorie/categorie.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieComponent implements OnInit {
 
+  optionToggled: boolean = false
   boitierIcon:string = '../../../assets/ICON/icons8-enclosure-for-servers-windows-11-color/icons8-enclosure-for-servers-48.png';
   carteMereIcon:string = '../../../assets/ICON/icons8-motherboard-color/icons8-motherboard-48.png';
   CPUIcon:string = '../../../assets/ICON/icons8-cpu-color/icons8-cpu-48.png';
@@ -15,6 +16,11 @@ export class CategorieComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(): void {
+    this.optionToggled = !this.optionToggled
+    console.log(this.optionToggled)
   }
 
 }
