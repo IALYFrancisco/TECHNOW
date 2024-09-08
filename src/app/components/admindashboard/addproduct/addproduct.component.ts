71395d2bdfee61dfa057097f0e49ac164831e76a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2024-09-08 11:32:29 +0300

    formulaire d'ajout de produit marche dans l'intrface client(sans contacte avec le serveur)

--- src/app/components/admindashboard/addproduct/addproduct.component.ts
+++ src/app/components/admindashboard/addproduct/addproduct.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addproduct',
@@ -10,7 +11,22 @@ export class AddproductComponent implements OnInit {
   constructor() { }
 
   @Input() _toggled:any;
-  
+
+  addProductForm = new FormGroup({
+    categorie: new FormControl('', [Validators.required]),
+    mark: new FormControl('', [Validators.required]),
+    model: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    color: new FormControl('', [Validators.required]),
+    stock: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+  })
+
+  getProductObject():void {
+    console.log(this.addProductForm.value)
+  }
+
   ngOnInit(): void {
   }
 
