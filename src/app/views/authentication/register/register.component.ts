8153fd4244c903f3294bf1e94f85e56320df8818
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-08 09:54:11 +0300

    fonctionnalité d'inscription développée

--- src/app/views/authentication/register/register.component.ts
+++ src/app/views/authentication/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,7 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   ngOnInit(): void {
   }
@@ -17,7 +18,16 @@ export class RegisterComponent implements OnInit {
     name: new FormControl(null, [Validators.required]),
     email: new FormControl(null, [Validators.required]),
     password: new FormControl(null, [Validators.required])
-  }) 
+  })
 
+  Register():void{
+
+    let _response
+
+    if(this.newUser.valid){
+      this.http.post('http://localhost:3000/authentication/register', this.newUser.value)
+        .subscribe((_response: any) => { console.log(_response) })
+    }
+  }
 
 }
