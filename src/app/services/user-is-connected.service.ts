cf1b0412b5d2a66c18fc97e5a6879206a934e575
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 20:23:10 +0300

    création du fonction pour vérifier si l'utilisateur est connecté ou pas

--- src/app/services/user-is-connected.service.ts
+++ src/app/services/user-is-connected.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class UserIsConnectedService {
 
   constructor() { }
+
+  userIsConnected():boolean {
+    let accessToken: any = localStorage.getItem('accessToken') || null
+    let _user: any = localStorage.getItem('user') || null
+    let user = JSON.parse(_user)
+    if(user && accessToken){
+      return true
+    }else{
+      return false
+    }
+  }
+
 }
