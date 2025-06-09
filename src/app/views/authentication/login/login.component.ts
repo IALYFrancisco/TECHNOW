caf6206d329d0eca0339db6a096f3746e189a456
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 11:56:01 +0300

    ajout de fonctionnalité de connexion

--- src/app/views/authentication/login/login.component.ts
+++ src/app/views/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, public router: Router) { }
 
   ngOnInit(): void {
   }
+
+  user:any = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required])
+  })
+
+  Login():void{
+    if(this.user.valid){
+      this.http.post('http://localhost:3000/authentication/login', this.user.value)
+        .subscribe((response:any) => { 
+          if(response.status == 200){
+            this.router.navigate(['/'])
+          }
+         })
+    }
+  }
+
+
 
 }
