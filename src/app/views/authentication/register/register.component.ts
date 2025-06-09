9870aff347f1eb23aee8e6e8c2b396593abab64e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 11:11:33 +0300

    ajout de redirection vers la page de connexion après l'inscription

--- src/app/views/authentication/register/register.component.ts
+++ src/app/views/authentication/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient, public router: Router ) { }
+
+  result:any
 
   ngOnInit(): void {
   }
@@ -23,7 +26,12 @@ export class RegisterComponent implements OnInit {
   Register():void{
     if(this.newUser.valid){
       this.http.post('http://localhost:3000/authentication/register', this.newUser.value)
-        .subscribe((response: any) => { console.log(response.status) })
+        .subscribe((response: any) => { 
+          this.result = response;
+          if(this.result.status == 201){
+            this.router.navigate(['/authentication/login'])
+          }
+        })
     }
   }
 
