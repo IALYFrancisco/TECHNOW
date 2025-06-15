078e5651fe585157db91d4bf805b0de3a674512e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-15 08:34:38 +0300

    amélioration du service

--- src/app/services/user-is-connected.service.ts
+++ src/app/services/user-is-connected.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,13 @@ export class UserIsConnectedService {
   constructor( private http: HttpClient ) { }
 
   async userIsConnected(): Promise<boolean> {
-    let accessToken: any = localStorage.getItem('accessToken') || null
+    let at: any = localStorage.getItem('accessToken') || null
+
+    this.http.post(`${environment.API_BASE_URL}/authentication/token/verify`, { accessToken: at }, { withCredentials: true })
+
     let _user: any = localStorage.getItem('user') || null
     let user = JSON.parse(_user)
-    if(user && accessToken){
+    if(user && at){
       return true
     }else{
       return false
