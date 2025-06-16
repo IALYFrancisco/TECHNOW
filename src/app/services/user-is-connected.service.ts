d7aa9f3ad88dfa552ccd719eb4b9d141560fade6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-16 11:36:05 +0300

    service refresh développé

--- src/app/services/user-is-connected.service.ts
+++ src/app/services/user-is-connected.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,6 +10,16 @@ export class UserIsConnectedService {
 
   constructor( private http: HttpClient ) { }
 
-  
+  refresh(): Observable<any>{
+    return this.http.post<{ accessToken: string }>(
+      `${environment}/authentication/token`,
+      {},
+      { withCredentials: true }
+    ).pipe(
+      tap((resposne) => { 
+        localStorage.setItem('accessToken', resposne.accessToken)
+       })
+    )
+  }
 
 }
