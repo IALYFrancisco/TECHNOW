07aff2c18e1c8ab6abafdd0a8179137dcd36e9b8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-14 16:20:34 +0300

    développement du fonctionnalité de récupération de liste de produit

--- src/app/components/admindashboard/adminallproducts/adminallproducts.component.ts
+++ src/app/components/admindashboard/adminallproducts/adminallproducts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-adminallproducts',
@@ -7,8 +8,12 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class AdminallproductsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/product/get').subscribe({
+      next: response => console.log(response),
+      error: () => console.log("Error fetching product list.")
+    })
   }
 }
