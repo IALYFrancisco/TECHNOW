c49d88d8dc54518fa85b4774ea88a36f5e43fa4f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-16 13:42:38 +0300

    page de connexion bien intéractive

--- src/app/views/authentication/login/login.component.ts
+++ src/app/views/authentication/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent {
   constructor(private http: HttpClient, public router: Router, private connection: UserIsConnectedService) { }
 
   formIsValid:any
+  wrongCredentials:any
 
   user:any = new FormGroup({
     email: new FormControl(null, [Validators.required]),
@@ -22,6 +23,7 @@ export class LoginComponent {
   })
 
   Login():void{
+    this.wrongCredentials = false
     if(this.user.valid){
       this.formIsValid = true
       this.http.post(`${environment.API_BASE_URL}/authentication/login`, this.user.value, { withCredentials: true, observe: 'response' })
@@ -29,7 +31,7 @@ export class LoginComponent {
           next: (response:any) => {
             if(response.status===204){
               this.formIsValid = false
-              window.alert("Email or password incorrect.")
+              this.wrongCredentials = true
             }if(response.status===200){
               localStorage.setItem('accessToken', response.body.accessToken)
               localStorage.setItem('user', JSON.stringify(response.body.user))
