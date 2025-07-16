8cf2677d6e9b8287b4f6a5d162b581693ddf52fe
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-16 15:47:34 +0300

    création de méthode items(), méthode retournant le nombre d'élément dans le panier

--- src/app/services/add-to-cart.service.ts
+++ src/app/services/add-to-cart.service.ts
@@ -6,6 +6,15 @@ import { Injectable } from '@angular/core';
 export class AddToCartService {
   
   constructor() { }
+
+  items():number {
+    let items = localStorage.getItem('cart') || null
+    if(items){
+      return items.length
+    }else{
+      return 0 
+    }
+  }
   
   Add(project:any):void {
     let cart = localStorage.getItem('cart') || null
