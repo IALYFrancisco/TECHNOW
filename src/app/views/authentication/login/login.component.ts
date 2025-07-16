e86f1e8a3352ddb8b13165bd6e89ccd936acb45c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-16 11:49:49 +0300

    ajout de plus d'intéraction dans la page de connexion

--- src/app/views/authentication/login/login.component.ts
+++ src/app/views/authentication/login/login.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserIsConnectedService } from 'src/app/services/user-is-connected.service';
@@ -10,12 +10,11 @@ import { environment } from 'src/environments/environment';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   constructor(private http: HttpClient, public router: Router, private connection: UserIsConnectedService) { }
 
-  ngOnInit(): void {
-  }
+  formIsValid:any
 
   user:any = new FormGroup({
     email: new FormControl(null, [Validators.required]),
@@ -24,6 +23,7 @@ export class LoginComponent implements OnInit {
 
   Login():void{
     if(this.user.valid){
+      this.formIsValid = true
       this.http.post(`${environment.API_BASE_URL}/authentication/login`, this.user.value, { withCredentials: true, observe: 'response' })
         .subscribe((response:any) => { 
           if(response.status == 200){
