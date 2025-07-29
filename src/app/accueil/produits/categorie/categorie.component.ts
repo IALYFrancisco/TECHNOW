dc6d85507176f9e90c11440acf8f571894686ad5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-29 20:11:50 +0300

    suppréssion des icones procésseur

--- src/app/accueil/produits/categorie/categorie.component.ts
+++ src/app/accueil/produits/categorie/categorie.component.ts
@@ -11,7 +11,6 @@ export class CategorieComponent implements OnInit {
   priceOptionToggled: boolean = true
   boitierIcon:string = '../../../assets/ICON/icons8-enclosure-for-servers-windows-11-color/icons8-enclosure-for-servers-48.png';
   carteMereIcon:string = '../../../assets/ICON/icons8-motherboard-color/icons8-motherboard-48.png';
-  CPUIcon:string = '../../../assets/ICON/icons8-cpu-color/icons8-cpu-48.png';
   GPUIcon:string = '../../../assets/ICON/icons8-video-card-color/icons8-video-card-48.png';
   RAMIcon:string = '../../../assets/ICON/icons8-ram-wanicon-lineal-color/icons8-ram-64.png';
   constructor() { }
