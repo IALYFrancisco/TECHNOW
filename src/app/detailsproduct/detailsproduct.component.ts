fcaf162c90e725e76b16689f23cb6a32f7e018b4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-08-02 11:12:53 +0300

    développement de la fonctionnalité de de récupération détails produit

--- src/app/detailsproduct/detailsproduct.component.ts
+++ src/app/detailsproduct/detailsproduct.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-detailsproduct',
@@ -8,9 +10,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsproductComponent implements OnInit {
 
-  constructor( private activeRoute: ActivatedRoute ) { }
+  constructor( private activeRoute: ActivatedRoute, private http: HttpClient ) { }
+
+  product: any;
 
   ngOnInit(): void {
+    this.http.get(`${environment}/product/get`, ).subscribe({
+      next: (response)=>{}
+    })
     console.log(this.activeRoute.snapshot.paramMap.get('id'))
   }
 
