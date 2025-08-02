d1af28d90a31811bea522cfe6f0e23f514bee8a8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-08-02 10:06:23 +0300

    configuration encore de la page détails produit

--- src/app/detailsproduct/detailsproduct.component.ts
+++ src/app/detailsproduct/detailsproduct.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailsproduct',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsproductComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activeRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    
   }
 
 }
