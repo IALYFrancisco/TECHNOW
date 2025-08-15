7b915bc8dc74500a294c9346e072bda633105b16
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-08-15 09:48:05 +0300

    dynamisation de la page panier des utilisateurs

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AddToCartService } from '../services/add-to-cart.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor( private cart: AddToCartService ) { }
+
+  apiBaseUrl = environment.API_BASE_URL
+
+  cart$ = this.cart.cartItems$
 
   ngOnInit(): void {
   }
